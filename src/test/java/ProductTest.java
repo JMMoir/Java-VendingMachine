37434bcea8076a46fb3b5662c7f6a0cd15e6d4ab
JMMoir <jmmoir@outlook.com>
2019-06-07 14:09:45 +0100

    poduct created, tests all passing

---
+++ src/test/java/ProductTest.java
@@ -0,0 +1,26 @@
+import Product.Product;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ProductTest {
+
+    private Product product;
+
+    @Before
+
+    public void setUp() {
+        product = new Product("Cola", 100);
+    }
+
+    @Test
+    public void canGetProductName() {
+        assertEquals("Cola", product.getName());
+    }
+
+    @Test
+    public void canGetProductPrice() {
+        assertEquals(100, product.getCost());
+    }
+}
